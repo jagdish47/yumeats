7a3659c519951eaae13d01fc79a0b04d48da75a1
jagdish47 <jagdishkumawat81@gmail.com>
2023-09-16 22:37:08 +0530

    completed the project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Featured from "./components/Featured";
 import Delivery from "./components/Delivery";
 import TopPicks from "./components/TopPicks";
 import Meal from "./components/Meal";
+import Categories from "./components/Categories";
+import NewsLetter from "./components/NewsLetter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Delivery />
       <TopPicks />
       <Meal />
+      <Categories />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 }
